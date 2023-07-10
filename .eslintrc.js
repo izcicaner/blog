04d7e16efb94c8fe71cd08dc5f20a60beadd4fe4
izcicaner <izcicaner@protonmail.com>
2023-07-10 17:20:00 +0300

    fix: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
+    '@next/next/no-page-custom-font': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
