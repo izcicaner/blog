395375100565828ce7754fb939af422d2dbbf36b
izcicaner <izcicaner@protonmail.com>
2023-07-15 17:18:04 +0300

    fix(build): remove snippets

--- pages/snippets/index.tsx
+++ pages/snippets/index.tsx
@@ -1,23 +1,23 @@
 import { PageSeo } from 'components/SEO'
 import { siteMetadata } from '~/data/siteMetadata'
-import { SnippetLayout } from '~/layouts/SnippetLayout'
+// import { SnippetLayout } from '~/layouts/SnippetLayout'
 import { getAllFilesFrontMatter } from '~/libs/mdx'
 import type { SnippetFrontMatter } from '~/types'
 
 export async function getStaticProps() {
-  let snippets = getAllFilesFrontMatter('snippets')
-  return { props: { snippets } }
+  // let snippets = getAllFilesFrontMatter('snippets')
+  // return { props: { snippets } }
 }
 
 export default function Snippet({ snippets }: { snippets: SnippetFrontMatter[] }) {
-  let description = 'Reuseable code snippets collected by me'
-  return (
-    <>
-      <PageSeo
-        title={`Snippets - ${siteMetadata.author} - ${siteMetadata.title}`}
-        description={description}
-      />
-      <SnippetLayout snippets={snippets} description={description} />
-    </>
-  )
+  // let description = 'Reuseable code snippets collected by me'
+  // return (
+  //   <>
+  //     <PageSeo
+  //       title={`Snippets - ${siteMetadata.author} - ${siteMetadata.title}`}
+  //       description={description}
+  //     />
+  //     <SnippetLayout snippets={snippets} description={description} />
+  //   </>
+  // )
 }
